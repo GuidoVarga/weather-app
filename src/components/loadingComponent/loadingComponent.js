7403ddbf1bafb6687b6594ad1eff31ac4c3a37fe
GuidoVarga <guidovargamartinez123@gmail.com>
2019-09-21 13:43:01 -0300

    animation - wip

--- src/components/loadingComponent/loadingComponent.js
+++ src/components/loadingComponent/loadingComponent.js
@@ -3,12 +3,33 @@ import './loadingComponent.scss';
 
 class LoadingComponent extends React.PureComponent {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            icon: "sun"
+        }
+    }
+
+
+    componentDidMount() {
+        setInterval(() => {
+            if(this.state.icon === "sun"){
+                this.setState({icon: "cloud"});
+            } else {
+                this.setState({icon: "sun"});
+            }
+        }, 2000);
+    }
+
 
     render() {
+        const icon = this.state.icon;
         return (
             <div className="loading-component">
-                <div className="icon-container">
-                    <i className="icon icon-sun"/>
+                <div className="loading-component__half-circle">
+                    <div className="icon-container">
+                        <i className={`icon icon-${icon}`}/>
+                    </div>
                 </div>
             </div>
         )
