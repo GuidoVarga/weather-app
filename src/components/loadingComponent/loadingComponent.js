54f910a744fb1e9890412bfce782387771fba153
GuidoVarga <guidovargamartinez123@gmail.com>
2019-09-22 18:41:37 -0300

    animation loader - wip

--- src/components/loadingComponent/loadingComponent.js
+++ src/components/loadingComponent/loadingComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getRandomIcon } from '../../helpers/loadingComponentHelper';
 import './loadingComponent.scss';
 
 class LoadingComponent extends React.PureComponent {
@@ -6,19 +7,42 @@ class LoadingComponent extends React.PureComponent {
     constructor(props){
         super(props);
         this.state = {
-            icon: "sun"
+            icon: "",
+            counter: 1,
+            interval: null
         }
     }
 
 
     componentDidMount() {
-        setInterval(() => {
-            if(this.state.icon === "sun"){
-                this.setState({icon: "cloud"});
-            } else {
-                this.setState({icon: "sun"});
+        this.changeIcon();
+        this.createInterval();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
+    createInterval = () => {
+        const interval = setInterval(() => {
+            if(this.state.counter < 6){
+                this.setState(prevState => ({
+                    counter: prevState.counter + 1
+                }))
+                }
+            else {
+                this.setState({counter: 1});
             }
-        }, 2000);
+            this.changeIcon();
+        }, 740);
+
+        this.setState({interval});
+    }
+
+    changeIcon = () => {
+        const { counter, icon } = this.state;
+        const newIcon = getRandomIcon(counter, icon);
+        this.setState({icon: newIcon});
     }
 
 
@@ -26,6 +50,9 @@ class LoadingComponent extends React.PureComponent {
         const icon = this.state.icon;
         return (
             <div className="loading-component">
+                <div className="loading-component__text">
+                    LOADING
+                </div>
                 <div className="loading-component__half-circle">
                     <div className="icon-container">
                         <i className={`icon icon-${icon}`}/>
