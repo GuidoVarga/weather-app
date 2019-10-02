87ca30ce4fe3ed9f899d35a00a5c72f74846decc
Guido Varga Martinez <guido.varga@globant.com>
2019-10-02 13:42:26 -0300

    tests

--- src/actions/searchActions.test.js
+++ src/actions/searchActions.test.js
@@ -3,15 +3,24 @@ import actionTypes from '../constants/actionTypes';
 
 describe('search actions test', () => {
 
-  const data = 'France';
-  const expectedAction = {
-      type: actionTypes.SET_SEARCH_COUNTRY,
-      payload: data
-  }
-
   it('should create an action to set the country to search', () => {
+      const data = 'France';
+      const expectedAction = {
+          type: actionTypes.SET_SEARCH_COUNTRY,
+          payload: data
+      }
+
       expect(searchActions.setSearchCountry(data)).toEqual(expectedAction);
   });
 
+  it('should create an action to set the city to search', () => {
+    const data = 'London';
+    const expectedAction = {
+        type: actionTypes.SET_SEARCH_CITY,
+        payload: data
+    }
+
+    expect(searchActions.setSearchCity(data)).toEqual(expectedAction);
+  });
 
 });
